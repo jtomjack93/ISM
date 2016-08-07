8662895dd09c90fbe243d964ea45d759e0ef35c0
jtomjack93 <jtomjack93@gmail.com>
2016-08-07 16:10:14 -0400

    Changes to install script

--- ServerInstall.sh
+++ ServerInstall.sh
@@ -3,15 +3,14 @@
 echo "172.23.135.68 intelligentsm.net" >> /etc/hosts
 cp ./* ../
 
-useradd nick
+adduser nick
 usermod -G sudo,www-data nick
-echo 'Password for nick:'
-passwd nick
 
-useradd tom
+adduser tom
 usermod -G sudo,www-data tom
-echo 'Password for tom:'
-passwd tom
+
+adduser adrian
+usermod -G sudo,www-data adrian
 
 wget https://intelligentsm.net/html.tar.gz
 wget https://intelligentsm.net/mysql.sql
@@ -23,6 +22,9 @@ wget https://intelligentsm.net/intelligentsm.key
 wget https://intelligentsm.net/intermediate.crt
 wget https://intelligentsm.net/apache2.tar.gz
 wget https://intelligentsm.net/postfix.tar.gz
+wget https://intelligentsm.net/ism.tar.gz
+wget https://intelligentsm.net/git.tar.gz
+wget https://intelligentsm.net/wpconfig.php
 
 a2enmod ssl
 
@@ -40,6 +42,7 @@ apt-get -y install ghostscript
 apt-get -y install unzip
 apt-get -y install fail2ban
 apt-get -y install postfix
+apt-get -y install git
 postmap /etc/postfix/virtual
 mv -i /etc/php5/conf.d/mcrypt.ini /etc/php5/mods-available/
 php5enmod mcrypt
@@ -54,12 +57,17 @@ mysql -u root -p'america!1' wordpress < wordpress.sql
 tar -zxvf html.tar.gz
 tar -zxvf apache2.tar.gz
 tar -zxvf postfix.tar.gz
+tar -zxvf ism.tar.gz
+tar -zxvf git.tar.gz
 rm -r /var/www/html
 mv var/www/html /var/www/
+mv wp-config.php /var/www/
 rm -r /etc/apache2
 mv etc/apache2 /etc/
 rm -r /etc/postfix
 mv etc/postfix /etc/
+mv opt/ism /opt/ism
+mv opt/ansur/asign/asign-online/.git /opt/ansur/asign/asign-online/.git
 
 
 ./asign-server-5.1-ubuntu-14.04-x86_64.sh
@@ -72,19 +80,24 @@ chmod 644 /usr/local/bin/frep
 chmod +x /usr/local/bin/frep
 echo "Enter full new domain name: "
 read domain
+echo "Enter asign domain name: "
+read asignDomain
 frep -h localhost -n wordpress -u root -p'america!1' -s intelligentsm.net -r $domain
+frep -h localhost -n wordpress -u root -p'america!1' -s 'asign.'$domain -r $asignDomain
 
 SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
-sed -i.old -r '/define\S\S[A-Z]{4}[A-Z]?[A-Z]?_([A-Z]{4})?([A-Z]{2})?_?(KEY|SALT)/d' /var/www/html/wp-config.php
+sed -i.old -r '/define\S\S[A-Z]{4}[A-Z]?[A-Z]?_([A-Z]{4})?([A-Z]{2})?_?(KEY|SALT)/d' /var/www/wp-config.php
 printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /var/www/html/wp-config.php
 
-sed -i -e 's/intelligentsm.net/'$domain'/g' /var/www/html/wp-config.php
+sed -i -e 's/intelligentsm.net/'$domain'/g' /var/www/wp-config.php
 sed -i -e 's/intelligentsm.net/'$domain'/g' /etc/apache2/sites-available/000-default.conf
-sed -i -e 's/intelligentsm.net/'$domain'/g' /etc/apache2/sites-available/asign.conf
+sed -i -e 's/intelligentsm.net/'$asignDomain'/g' /etc/apache2/sites-available/asign.conf
 sed -i -e 's/intelligentsm.net/'$domain'/g' /etc/postfix/main.cf
 
-chown -R www-data:www-data /var/www/html
-chmod -R g+w /var/www/html
+chown -R www-data:www-data /var/www
+chmod -R g+w /var/www
+
+echo "*/5 * * * * /opt/ism/dbsync.sh" >> /var/spool/cron/crontabs/root
 
 #a2ensite 000-default.conf
 #a2ensite asign.conf
